e91a736a91d7e00fee89e395c5959e05b674fd75
Ronald Lobo <ronald.lobo@pointclearsolutions.com>
2015-09-19 11:15:25 -0600

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serviApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the serviApp
+ */
+angular.module('serviApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
